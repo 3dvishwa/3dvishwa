901aeca7d50efc07ce4c9de0967df10822a8bf12
deepak gaikwad <deepaksgaikwad86@gmail.com>
2026-07-31 09:32:49 +0530

    changes done for vercel deployment

--- 3d-print-store/src/app/login/page.tsx
+++ 3d-print-store/src/app/login/page.tsx
@@ -54,6 +54,7 @@ export default function LoginPage() {
             headers: { "Content-Type": "application/json" },
             credentials: "include",
             body: JSON.stringify({ idToken }),
+
         });
 
         if (!res.ok) {
